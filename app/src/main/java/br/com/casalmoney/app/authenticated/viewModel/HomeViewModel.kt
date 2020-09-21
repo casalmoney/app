8ed03f940178b3b0d3047969777502ad326419b2
tiagorc <eulercarvalho.se@gmail.com>
2020-09-20 21:04:23 -0300

    Filling up correct name and initials

--- app/src/main/java/br/com/casalmoney/app/authenticated/viewModel/HomeViewModel.kt
+++ app/src/main/java/br/com/casalmoney/app/authenticated/viewModel/HomeViewModel.kt
@@ -3,13 +3,28 @@ package br.com.casalmoney.app.authenticated.viewModel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import br.com.casalmoney.app.authenticated.interactor.HomeInteractor
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 
 open class HomeViewModel(val app: Application) : AndroidViewModel(app) {
 
     val homeInteractor = HomeInteractor()
+    val currentUser = FirebaseAuth.getInstance().currentUser
 
     fun logout() {
         homeInteractor.logout()
     }
 
+    val nameInitials: String
+        get() {
+            val initials = currentUser?.displayName.toString()
+                .split(' ')
+                .mapNotNull { it.firstOrNull()?.toString() }
+                .reduce { acc, s -> acc + s }
+            return if (initials.length == 1) {
+                initials + initials
+            } else {
+                initials.substring(0, 2)
+            }
+        }
 }
