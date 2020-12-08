9fc0a4f9cc88a161c51a87bf02823f8413226190
Gabriel Lucas <g.lucas.santos@gmail.com>
2020-12-07 21:55:46 -0300

    open browser

--- app/src/main/java/br/com/casalmoney/app/authenticated/view/fragments/HelpFragment.kt
+++ app/src/main/java/br/com/casalmoney/app/authenticated/view/fragments/HelpFragment.kt
@@ -1,26 +1,24 @@
 package br.com.casalmoney.app.authenticated.view.fragments
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import br.com.casalmoney.app.R
+import br.com.casalmoney.app.authenticated.view.adapters.NewsAdapter
 import br.com.casalmoney.app.authenticated.viewModel.HelpViewModel
 import br.com.casalmoney.app.databinding.FragmentHelpBinding
-import br.com.casalmoney.app.authenticated.view.adapters.ChatAdapter
-import br.com.casalmoney.app.authenticated.view.adapters.NewsAdapter
-import br.com.casalmoney.app.authenticated.viewModel.HomeViewModel
 import br.com.casalmoney.app.utils.CustomProgressDialog
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.WithFragmentBindings
+
 
 @AndroidEntryPoint
 @WithFragmentBindings
@@ -61,7 +59,8 @@ class HelpFragment: Fragment() {
 
         viewModel.news.observe(viewLifecycleOwner, { list ->
             recyclerView.adapter = NewsAdapter(list) {
-                Log.d("CLICOU", "RESULTADO DO CLICK")
+                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(it.url))
+                startActivity(browserIntent)
             }
         })
 
